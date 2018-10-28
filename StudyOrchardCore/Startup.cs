c069f5f22af080fb801897de611ca644ede8e0ef
jaylan <jaylanchen@126.com>
2018-10-28 13:58:50 +0800

    外部json设置

--- StudyOrchardCore/Startup.cs
+++ StudyOrchardCore/Startup.cs
@@ -6,29 +6,38 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace StudyOrchardCore
 {
     public class Startup
     {
+
+        public IConfiguration Configuration { get; set; }
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddOrchardCms();
 
+            //services.Configure<IdentityOptions>(options =>
+            //{
+            //    options.Password.RequireDigit = false;
+            //    options.Password.RequireLowercase = true;
+            //    options.Password.RequireUppercase = true;
+            //    options.Password.RequireNonAlphanumeric = false;
+            //    options.Password.RequiredUniqueChars = 3;
+            //    options.Password.RequiredLength = 6;
+
+            //    options.SignIn.RequireConfirmedEmail = false;
+            //    options.SignIn.RequireConfirmedPhoneNumber = false;
+            //});
             services.Configure<IdentityOptions>(options =>
             {
-                options.Password.RequireDigit = false;
-                options.Password.RequireLowercase = true;
-                options.Password.RequireUppercase = true;
-                options.Password.RequireNonAlphanumeric = false;
-                options.Password.RequiredUniqueChars = 3;
-                options.Password.RequiredLength = 6;
-
-                options.SignIn.RequireConfirmedEmail = true;
-                options.SignIn.RequireConfirmedPhoneNumber = true;
+                Configuration.GetSection("IdentityOptions").Bind(options);
             });
         }
 
@@ -44,6 +53,14 @@ namespace StudyOrchardCore
             //{
             //    await context.Response.WriteAsync("Hello World!");
             //});
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsetting.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
         }
     }
 }
