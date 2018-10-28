57bf3f0446480c2beb2aaf068285efbb14775cc9
jaylan <jaylanchen@126.com>
2018-10-28 13:35:57 +0800

    add Module

---
+++ Study.MyModule/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using OrchardCore.Modules;
+
+namespace Study.MyModule
+{
+    public class Startup: StartupBase
+    {
+        public override void ConfigureServices(IServiceCollection services)
+        {
+        }
+    }
+}
